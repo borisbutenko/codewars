3fec98dffcd0f19163a56aa6e6069e767026ea85
iamborisbutenko <b.butenko@brandquad.ru>
2018-12-12 17:26:04 +0400

    add test for likes function

--- likes/test.js
+++ likes/test.js
@@ -1,9 +1,23 @@
-describe('example tests', function() {
-  it('should return correct text', function() {
-    Test.assertEquals(likes([]), 'no one likes this');
-    Test.assertEquals(likes(['Peter']), 'Peter likes this');
-    Test.assertEquals(likes(['Jacob', 'Alex']), 'Jacob and Alex like this');
-    Test.assertEquals(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this');
-    Test.assertEquals(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this');
-  });
-});
+describe('"Likes" function test for result equals', function() {
+
+  it('Empty array', function() {
+    assert.equal(likes([]), 'no one likes this')
+  })
+
+  it('One name', function() {
+    assert.equal(likes(['Peter']), 'Peter likes this')
+  })
+
+  it('Two names', function() {
+    assert.equal(likes(['Jacob', 'Alex']), 'Jacob and Alex like this')
+  })
+
+  it('Three names', function() {
+    assert.equal(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this')
+  })
+
+  it('More names', function() {
+    assert.equal(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this')
+  })
+
+})
