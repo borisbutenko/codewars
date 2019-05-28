dee5633a6df4eafd2efd882d69704f76b3208cc0
borisbutenko <iamborisbutenko@gmail.com>
2019-05-28 04:33:24 +0400

    add find_it

---
+++ src-python/find_it.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+def find_it(lst: List) -> int or None:
+    """
+    Given an array, find the int that appears an odd number of times.
+    There will always be only one integer that appears an odd number of times.
+
+    `Kata <https://www.codewars.com/kata/54da5a58ea159efa38000836/train/python>`
+
+    :param lst: Array of numbers
+    :type lst: list
+    :return {int}:
+    """
+    numbers = set(lst)
+
+    for num in numbers:
+        count = lst.count(num)
+        if count % 2 != 0:
+            return num
+
+    return None
